07a01b6aa6e05b12f19d433a91690adaa77895c5
MAB75013 <abdellaoui.mehdi@gmail.com>
2026-06-08 13:19:11 +0200

    Delete page.tsx

--- page.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-import { useAppStore } from '../lib/store';
-import LoginPage from '../components/pages/LoginPage';
-import AppShell from '../components/AppShell';
-
-export default function Home() {
-  const { currentUser } = useAppStore();
-  return currentUser ? <AppShell /> : <LoginPage />;
-}
